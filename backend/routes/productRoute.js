b470f533c4e5ee3f13d5ea7005123654f7e84178
Gabriel <sgabrielok01@gmail.com>
2022-10-03 04:35:13 -0300

    ruta de producto detalle agregada

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,11 +5,16 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  getProductDetails,
 } = require("../controllers/productController");
 
 router.get("/products", getAllProducts);
+router.get("/product/:id", getProductDetails);
+
 router.post("/products/new", createProduct);
+
 router.put("/product/:id", updateProduct);
+
 router.delete("/product/:id", deleteProduct);
 
 module.exports = router;
