e2feeb3159e6fe05972980bce1d3f7c24fdaa9ea
Gabriel <sgabrielok01@gmail.com>
2022-10-03 22:02:06 -0300

    configuraciones de errores

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./database");
 
+// Handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Uncaught Exception");
+  process.exit(1);
+});
+
 // Config
 dotenv.config({ path: "backend/config/config.env" });
 
