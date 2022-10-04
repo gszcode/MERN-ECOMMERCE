e555621481f9a90ac8629c3c0abbaca8f308bdd1
Gabriel <sgabrielok01@gmail.com>
2022-10-03 22:01:18 -0300

    manejador de errors agregado

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const errorMiddleware = require("./middlewares/error");
 const app = express();
 
 app.use(express.json());
 
 // Route imports
 const product = require("./routes/productRoute");
-
 app.use("/api/v1", product);
+
+// Middleware for errors
+app.use(errorMiddleware);
+
+// app.use((error, req, res, next) => {
+//   console.error(error.message);
+
+//   return res.status(404).json({
+//     success: false,
+//     message: error.message,
+//   });
+// });
 
 module.exports = app;
