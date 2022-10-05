55b9ae3e6324dec4e62b4f284f85da5d3ea1c7a2
Gabriel <sgabrielok01@gmail.com>
2022-10-05 04:24:16 -0300

    ruta de olvide contraseña y resear contrasela creadas

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,14 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:id", resetPassword);
 router.get("/logout", logoutUser);
 
 module.exports = router;
