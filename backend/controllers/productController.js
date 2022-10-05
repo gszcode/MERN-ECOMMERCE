1acde7dd808d70371b9b12bd73b4a5e0582390d3
Gabriel <sgabrielok01@gmail.com>
2022-10-05 04:22:51 -0300

    controladores de envio de token y resetear contraseña

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ const ApiFeatures = require("../utils/apiFeatures");
 
 // Create product
 exports.createProduct = catchAsyncError(async (req, res, next) => {
+  req.body.user = req.user.id;
+
   const product = await Product.create(req.body);
 
   return res.status(201).json({
