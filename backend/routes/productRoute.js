2ff5949d2763b19fcdb00c6bb9f2d41bb1fbea1c
Gabriel <sgabrielok01@gmail.com>
2022-10-06 04:59:00 -0300

    rutas: obtener, editar y eliminar reviews

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -7,30 +7,38 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
 
+// all products and their details
 router.get("/products", getAllProducts);
 router.get("/product/:id", getProductDetails);
 
+// create product - update product - delete product
 router.post(
-  "/products/new",
+  "/admin/products/new",
   isAuthenticatedUser,
   authorizeRoles("admin"),
   createProduct
 );
-
 router.put(
-  "/product/:id",
+  "/admin/product/:id",
   isAuthenticatedUser,
   authorizeRoles("admin"),
   updateProduct
 );
-
 router.delete(
-  "/product/:id",
+  "/admin/product/:id",
   isAuthenticatedUser,
   authorizeRoles("admin"),
   deleteProduct
 );
+
+// get, create, update and delete review
+router.get("/reviews", getProductReviews);
+router.put("/review", isAuthenticatedUser, createProductReview);
+router.delete("/reviews", isAuthenticatedUser, deleteReview);
 
 module.exports = router;
