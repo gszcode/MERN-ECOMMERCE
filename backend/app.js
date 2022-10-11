6ea7a3a6378beee0217be12baec61bbd769b6914
Gabriel <sgabrielok01@gmail.com>
2022-10-11 02:18:14 -0300

    ruta order agregada

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,14 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
 
-// Route imports
+// Routes imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // Middleware for errors
 app.use(errorMiddleware);
